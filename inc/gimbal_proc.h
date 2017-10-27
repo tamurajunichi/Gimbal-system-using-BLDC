6e31c328812cdb7148493cd8e4291512c3383c66
tamura junichi <r201603849je@jindai.jp>
2017-10-27 01:04:24 +0000

    gimbal_proc.h を Bitbucket 上でオンラインで編集しました。

--- inc/gimbal_proc.h
+++ inc/gimbal_proc.h
@@ -23,18 +23,21 @@
 /* Variable -------------------------------------------------------------------*/
 uint32_t samp_Time,print_Time;
 float temp,angle_x,angle_y,angle_z,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z;
-float duty_phaseU,duty_phaseV,duty_phaseW;
+float duty_PhaseU,duty_PhaseV,duty_PhaseW;
 int   phaseU,phaseV,phaseW;
 
 /* Functions ------------------------------------------------------------------*/
 void data_init();
 void data_processing(uint32_t tim2_count);
-void calc_mpu6050();
+
+void proc_mpu6050();
 float proc_angle_z(float gyro_z);
 float proc_angle_x(float accel_x);
 float proc_angle_y(float accel_y);
+
 float get_angle_x();
 float get_angle_y();
+float get_angle_z();
 void generate_sine_pwm();
 
 #endif /* __GIMBAL_PROC_H */
